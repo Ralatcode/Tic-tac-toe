341de092b182f11b396509f927a07d0fc8a074ae
RalatPC <ralatrado@gmail.com>
2023-07-25 13:32:44 +0100

    rendered array values to board

--- app.js
+++ app.js
@@ -7,15 +7,25 @@ const gameBoard = (()=>{
         boardDiv.classList.add('boardDiv')
         for (i = 1; i <= 9; i++) {
             const box = document.createElement('div');
-            box.classList.add(`box-${i}`)
+            box.classList.add(`box-${i}`, 'box-div')
             boardDiv.append(box);
         }
         container.append(boardDiv);
     }
+
+    const renderBoard = () => {
+        buildBoard();
+        let boxes = document.querySelectorAll('.box-div');
+        boxes = Array.from(boxes);
+
+        for (i = 0; i < boxes.length; i++) {
+            boxes[i].textContent = boardContent[i]; 
+        }
+
+    }
     return {
-        buildBoard
+        renderBoard
     };
 })();
 
-gameBoard.buildBoard()
-
+gameBoard.renderBoard();
