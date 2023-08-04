c9b69d39bf4c072190871991793ea9ef8ee81747
RalatPC <ralatrado@gmail.com>
2023-08-04 02:05:24 +0100

    player factory and game controller module

--- app.js
+++ app.js
@@ -52,8 +52,97 @@ boxes.forEach((box) => {
 
 
 // winning logic
-const Board = [
-    ['X', 'O', 'X'],
-    ['O', 'X', 'O'],
-    ['X', 'O', 'X']
-];
+
+const example = () => {
+    const row = 3;
+    const column = 3;
+    const board = [];
+
+    for (let i = 0; i < row; i++) {
+        board[i] = [];
+        for (let j = 0; j < column; j++) {
+        board[i].push(Cell());
+        }
+    }
+
+    const markPlayerInput = (player, row, col) => {
+        const playerSpot = board[row][col];
+        // checks if cell is empty
+        if (playerSpot.getValue() == 0) {  // convert to ternary operator
+            playerSpot.addPlayerMark(player.token);
+        } else {
+            console.log('this cell is not empty');
+        }
+    }
+
+    const printBoard = () => {
+        const boardWithValue = board.map((row) => row.map((cell) => cell.getValue()))
+        console.log(boardWithValue)
+    }
+
+    return {markPlayerInput, printBoard};
+}
+
+
+
+function Cell() {
+    let value = 0;
+
+    const addPlayerMark = (player) => {
+        value = player;
+    };
+
+    const getValue = () => value;
+
+    return {addPlayerMark, getValue};
+}
+
+// player factory function
+const Players = (player, token) => {
+    let playerName = player;
+    const updatePlayerName = (playerInput) => playerName = playerInput;
+    
+    const getPlayerName = () => playerName;
+
+    return {player, token, getPlayerName, updatePlayerName};
+}
+
+
+const GameController = () => {
+    const playerOne = Players('Player-One', 'X');
+    const playerTwo = Players('Player-Two', 'O');
+
+    const players = [playerOne, playerTwo];
+
+    let activePlayer = players[0];
+
+    const board1 = example();
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0]; 
+    }
+
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () => {
+        board1.printBoard();
+    }
+
+    const playRound = (row, col) => {
+        board1.addPlayerMark(getActivePlayer(), row, col);
+
+        // Win logic
+        switchPlayerTurn();
+        printNewRound();
+    }
+    printNewRound();
+
+    return {playRound, getActivePlayer};
+}
+
+
+
+
+
+// console.log(board);
+
